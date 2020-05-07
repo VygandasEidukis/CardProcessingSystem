d1c007eec7271f9ef16ff7b84792717cb67324e6
vygandaseidukis <vygandaseidukis@gmail.com>
2020-05-07 08:33:44 +0300

    Prevent from closing while generating codes

--- Server/MainWindow.xaml.cs
+++ Server/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using Server.Model;
@@ -7,6 +8,8 @@ namespace Server
 {
     public partial class MainWindow : Window
     {
+        private bool _isLocked { get; set; }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -16,14 +19,25 @@ namespace Server
         {
             GenerateCodes.IsEnabled = false;
             var generateCodesTask = new Task(()=> CodeProcessing.GenerateDiscountCodes(1000));
+            _isLocked = true;
             generateCodesTask.ContinueWith(x =>
             {
+                _isLocked = false;
                 Application.Current.Dispatcher.Invoke(new Action(() =>
                 {
                     GenerateCodes.IsEnabled = true;
                 }));
             });
             generateCodesTask.Start();
+        }
+
+        //prevent from closing before generating is over
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            while (_isLocked)
+                Thread.Sleep(100);
+
+            e.Cancel = false;
         }
     }
 }
